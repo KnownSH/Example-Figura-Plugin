eb13e0926501f3332276457067df0891842d400a
KnownSH <71918238+KnownSH@users.noreply.github.com>
2024-04-05 04:25:42 -0700

    Update microphone.lua

--- examples/microphone.lua
+++ examples/microphone.lua
@@ -1,5 +1,5 @@
 if type(events["svc.microphone"]) == "Event" then
     events["svc.microphone"] = function ()
-        print("help")
+        print("player is currently talking on mic")
     end
 end
