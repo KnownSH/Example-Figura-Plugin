cb77c7125d6a45b22224dd2c6d584bff39e14d9f
KnownSH <xanp101@gmail.com>
2024-04-05 04:00:25 -0700

    Update SVCPlugin.java

--- common/src/main/java/net/knsh/figurasvc/SVCPlugin.java
+++ common/src/main/java/net/knsh/figurasvc/SVCPlugin.java
@@ -1,6 +1,7 @@
 package net.knsh.figurasvc;
 
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
+import de.maxhenkel.voicechat.api.events.ClientReceiveSoundEvent;
 import de.maxhenkel.voicechat.api.events.ClientSoundEvent;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import org.figuramc.figura.FiguraMod;
@@ -15,9 +16,19 @@ public class SVCPlugin implements VoicechatPlugin {
 
     @Override
     public void registerEvents(EventRegistration registration) {
+        registration.registerEvent(ClientReceiveSoundEvent.class, this::onPlayerSpeak);
         registration.registerEvent(ClientSoundEvent.class, this::onClientSpeak);
     }
 
+    // for other players
+    private void onPlayerSpeak(ClientReceiveSoundEvent event) {
+        Avatar avatar = AvatarManager.getAvatarForPlayer(event.getId());
+        if (!event.getVoicechat().isMuted()) {
+            avatar.run("SVC.MICROPHONE", avatar.tick);
+        }
+    }
+
+    // for the player who is speaking
     private void onClientSpeak(ClientSoundEvent event) {
         Avatar avatar = AvatarManager.getAvatarForPlayer(FiguraMod.getLocalPlayerUUID());
         if (!event.getVoicechat().isMuted()) {
