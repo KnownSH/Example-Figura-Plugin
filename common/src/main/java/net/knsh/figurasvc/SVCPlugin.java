ea76159a3fbbc202a03211f19cf4d21b68da4852
KnownSH <xanp101@gmail.com>
2024-04-05 15:21:03 -0700

    rewrite to use pings

--- common/src/main/java/net/knsh/figurasvc/SVCPlugin.java
+++ common/src/main/java/net/knsh/figurasvc/SVCPlugin.java
@@ -1,9 +1,8 @@
 package net.knsh.figurasvc;
 
+import de.maxhenkel.voicechat.api.VoicechatClientApi;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
-import de.maxhenkel.voicechat.api.events.ClientReceiveSoundEvent;
-import de.maxhenkel.voicechat.api.events.ClientSoundEvent;
-import de.maxhenkel.voicechat.api.events.EventRegistration;
+import de.maxhenkel.voicechat.api.events.*;
 import org.figuramc.figura.FiguraMod;
 import org.figuramc.figura.avatar.Avatar;
 import org.figuramc.figura.avatar.AvatarManager;
@@ -16,16 +15,7 @@ public class SVCPlugin implements VoicechatPlugin {
 
     @Override
     public void registerEvents(EventRegistration registration) {
-        registration.registerEvent(ClientReceiveSoundEvent.class, this::onPlayerSpeak);
         registration.registerEvent(ClientSoundEvent.class, this::onClientSpeak);
-    }
-
-    // for other players
-    private void onPlayerSpeak(ClientReceiveSoundEvent event) {
-        Avatar avatar = AvatarManager.getAvatarForPlayer(event.getId());
-        if (!event.getVoicechat().isMuted()) {
-            avatar.run("SVC.MICROPHONE", avatar.tick);
-        }
     }
 
     // for the player who is speaking
--- examples/microphone.lua
+++ examples/microphone.lua
@@ -1,5 +1,19 @@
+local microphoneOffTime = 0
+local isMicrophoneOn = false
+
+function events.tick()
+    local previousMicState = isMicrophoneOn
+
+    microphoneOffTime = microphoneOffTime + 1
+    isMicrophoneOn = microphoneOffTime <= 2
+
+    if previousMicState ~= isMicrophoneOn then
+        print(isMicrophoneOn and "Speaking" or "not speaking")
+    end
+end
+
 if type(events["svc.microphone"]) == "Event" then
     events["svc.microphone"] = function ()
-        print("help")
+        microphoneOffTime = 0
     end
 end
